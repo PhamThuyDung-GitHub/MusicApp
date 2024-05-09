48266885bc59077d1310b55c5c4279ffd91dd84e
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-05-09 22:26:30 +0700

    auth

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -2,11 +2,14 @@ import {
   create,
   generateForgetPasswordLink,
   grantValid,
+  logOut,
+  sendProfile,
   sendReVerificationToken,
-  signIn,
+  signIn,z
   updatePassword,
+  updateProfile,
   verifyEmail,
-} from "#/controllers/user";
+} from "#/controllers/auth";
 import { isValidPassResetToken, mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
 import {
@@ -16,7 +19,7 @@ import {
   UpdatePasswordSchema,
 } from "#/utils/validationSchema";
 import { Router } from "express";
-import fileParser, { RequestWithFiles } from "#/middleware/fileParser";
+import fileParser from "#/middleware/fileParser";
 
 const router = Router();
 
@@ -37,15 +40,9 @@ router.post(
   updatePassword
 );
 router.post("/sign-in", validate(SignInValidationSchema), signIn);
-router.get("/is-auth", mustAuth, (req, res) => {
-  res.json({
-    profile: req.user,
-  });
-});
+router.get("/is-auth", mustAuth, sendProfile);
 
-router.post("/update-profile", fileParser, (req: RequestWithFiles, res) => {
-  console.log(req.files);
-  res.json({ ok: true });
-});
+router.post("/update-profile", mustAuth, fileParser, updateProfile);
+router.post("/log-out", mustAuth, logOut);
 
 export default router;
