44d7d7495ccd048fc9e5cbf8c5f6023d11c7a916
PhamThuyDung-GitHub <20521214@gm.uit.edu.vn>
2024-05-11 06:44:52 +0700

    playlist

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -2,6 +2,7 @@ import { CreatePlaylistRequest, UpdatePlaylistRequest } from "#/@types/audio";
 import Audio from "#/models/audio";
 import Playlist from "#/models/playlist";
 import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
 
 export const createPlaylist: RequestHandler = async (
   req: CreatePlaylistRequest,
@@ -70,4 +71,59 @@ export const updatePlaylist: RequestHandler = async (
       visibility: playlist.visibility,
     },
   });
+};
+
+export const removePlaylist: RequestHandler = async (req, res) => {
+  const { playlistId, resId, all } = req.query;
+
+  if (!isValidObjectId(playlistId))
+    return res.status(422).json({ error: "Invalid playlist id!" });
+
+  if (all === "yes") {
+    const playlist = await Playlist.findOneAndDelete({
+      _id: playlistId,
+      owner: req.user.id,
+    });
+
+    if (!playlist)
+      return res.status(404).json({ error: "Playlist not found!" });
+  }
+
+  if (resId) {
+    if (!isValidObjectId(resId))
+      return res.status(422).json({ error: "Invalid audio id!" });
+
+    const playlist = await Playlist.findOneAndUpdate(
+      {
+        _id: playlistId,
+        owner: req.user.id,
+      },
+      {
+        $pull: { items: resId },
+      }
+    );
+
+    if (!playlist)
+      return res.status(404).json({ error: "Playlist not found!" });
+  }
+
+  res.json({ success: true });
+};
+
+export const getPlaylistByProfile: RequestHandler = async (req, res) => {
+  const data = await Playlist.find({
+    owner: req.user.id,
+    visibility: { $ne: "auto" },
+  }).sort("-createdAt");
+
+  const playlist = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      itemsCount: item.items.length,
+      visibility: item.visibility,
+    };
+  });
+
+  res.json({ playlist });
 };
